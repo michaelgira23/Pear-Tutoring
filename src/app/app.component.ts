796b50b42451eabf0c23893d851413a80527c2fb
Michael Gira <contact@michaelgira.me>
2016-11-29 12:21:10 -0600

    Listen to all changes in database for test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
 
-  constructor (private af: AngularFire) {
-    const rootList$: FirebaseListObservable<any> = af.database.list('wwt-hackathon-2016');
-    rootList$.subscribe(console.log);
-  }
+	constructor (private af: AngularFire) {
+		const rootList$: FirebaseListObservable<any> = af.database.list('/');
+		rootList$.subscribe(
+			data => {
+				console.log(data);
+				this.title = JSON.stringify(data);
+			}
+		);
+	}
 }
