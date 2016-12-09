1d5f64570a1b8405cb6f8b7b0343eb22f704566e
Nick Clifford <catcheese12345@gmail.com>
2016-12-09 15:12:02 -0600

    start chat service

---
+++ src/app/shared/model/chat.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, Inject } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import { AngularFireDatabase, FirebaseRef } from 'angularfire2';
+import { User } from './user';
+
+
+@Injectable()
+export class ChatService {
+
+	sdkDb: any;
+
+	constructor(@Inject(FirebaseRef) fb, private db: AngularFireDatabase) {
+		this.sdkDb = fb.database().ref();
+	}
+
+	getAllMessages(chatKey: string): Observable<any[]> {
+		return this.db.list('chatMessages', {
+			query: {
+				orderByChild: 'chat',
+				equalTo: chatKey
+			}
+		});
+	}
+
+	createChat(chat: any): Observable<any> {
+		let chatToSave = Object.assign({}, chat);
+		let newChatKey = this.sdkDb.child('chats').push().key;
+
+		let dataToSave = {};
+		dataToSave[`chats/${newChatKey}`] = chatToSave;
+		return this.firebaseUpdate(dataToSave);
+	}
+
+	saveMessage(message: Message): Observable<any> {
+		let msgToSave = Object.assign({}, message);
+		let newMsgKey = this.sdkDb.child('chatMessages').push().key;
+
+		let dataToSave = {};
+		dataToSave[`chatMessages/${newMsgKey}`] = msgToSave;
+		return this.firebaseUpdate(dataToSave);
+	}
+
+	private firebaseUpdate(dataToSave) {
+		const subject = new Subject();
+
+		this.sdkDb.update(dataToSave)
+			.then(
+				val => {
+					subject.next(val);
+					subject.complete();
+
+				},
+				err => {
+					subject.error(err);
+					subject.complete();
+				}
+			);
+
+		return subject.asObservable();
+	}
+}
+
+export interface Message {
+	chat: string,
+	text: string,
+	from: User
+}
