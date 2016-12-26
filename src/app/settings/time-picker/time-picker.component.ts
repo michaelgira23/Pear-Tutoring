f7c1d9fc6e54e77a661f6e590e8e0472071e32e9
AsianPsychoBoy <ziyang_99@outlook.com>
2016-12-26 14:59:22 +0800

    add timer picker functionalities
    style is broken

--- src/app/settings/time-picker/time-picker.component.ts
+++ src/app/settings/time-picker/time-picker.component.ts
@@ -7,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimePickerComponent implements OnInit {
 
+	get freeTimes(): {[key: string]: {from: number; to: number}[]} {
+		return this.freeTimesModel;
+	}
+	get freeTimesProps(): string[] {
+		return Object.getOwnPropertyNames(this.freeTimesModel);
+	}
+	freeTimesModel: any = {
+		Sunday: [],
+		Monday: [],
+		Tuesday: [],
+		Wednsday: [],
+		Thursday: [],
+		Friday: [],
+		Saturday: [],
+	};
+
 	constructor() { }
 
 	ngOnInit() {
 	}
 
+	addTime(day: string) {
+		this.freeTimesModel[day].push({from: '', to: ''});
+	}
+
+	removeTime(day: string, timeIndex: number) {
+		this.freeTimesModel[day].splice(timeIndex, 1);
+	}
+
 	// add - delete - store time periods in 7 days in a week
+	// @ViewChild can help access the times from settings component
 }
