ea5586bd90ca1d46b0f06a9424d0c40bc2b38bfc
Nick Clifford <catcheese12345@gmail.com>
2016-12-27 15:25:28 -0600

    start boilerplating, add permissions types

--- src/app/shared/model/permissions.service.ts
+++ src/app/shared/model/permissions.service.ts
@@ -6,6 +6,27 @@ export class PermissionsService {
 
 	constructor(private af: AngularFire) { }
 
+	createPermission(type: PermissionsType, permission: Permission) { }
+
+	updatePermission($key: string, permission: Permission) {}
+
 }
 
-export type PermissionsOptions = 'chat' | 'session' | 'whiteboard';
+
+export type PermissionsType = 'chat' | 'session' | 'whiteboard';
+
+export interface Permission {
+	[$key: string]: {
+		anonymous: Partial<PermissionsScopes>;
+		loggedIn: Partial<PermissionsScopes>;
+		user: {
+			[$uid: string]: Partial<PermissionsScopes>;
+		}
+	};
+};
+
+export interface PermissionsScopes {
+	read: boolean;
+	write: boolean;
+	moderator: boolean;
+};
