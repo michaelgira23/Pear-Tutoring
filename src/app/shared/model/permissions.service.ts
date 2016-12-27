98d50d87c8a90884c7bff622d5599a212297fb5b
Nick Clifford <catcheese12345@gmail.com>
2016-12-27 16:47:18 -0600

    add some functionality, renaming

--- src/app/shared/model/permissions.service.ts
+++ src/app/shared/model/permissions.service.ts
@@ -1,27 +1,74 @@
 import { Injectable } from '@angular/core';
 import { AngularFire } from 'angularfire2';
+import { Observable, Subject } from 'rxjs/Rx';
 
 @Injectable()
 export class PermissionsService {
 
 	constructor(private af: AngularFire) { }
 
-	createPermission(type: PermissionsType, permission: Permission) { }
+	createPermission($key: string, type: PermissionsType, permission: Permission): Observable<any> {
+		const permissions = this.af.database.list(`${type}Permissions`);
+		const permObj = {};
+		permObj[$key] = permission;
 
-	addScope($key: string, scope: Partial<PermissionsScopes>, group: PermissionsGroups, $uid?: string) {
+		return this.observableToPromise(permissions.push(permObj));
+	}
+
+	// We have to ask for the $key and type again since that's how we organize permissions.
+	addScope(
+		$key: string,
+		type: PermissionsType,
+		scopes: Partial<PermissionsScopes>,
+		group: PermissionsGroup,
+		$uid?: string
+	): Observable<any> {
 		if (group === 'user') {
-			// add scope, with $uid handling
+			const users = this.af.database.list(`${type}Permissions/${$key}/user`);
+			const scopeObj = {};
+			scopeObj[$uid] = scopes;
+
+			return this.observableToPromise(users.push(scopeObj));
+		} else {
+			const permissions = this.af.database.list(`${type}Permissions/${$key}`);
+			const scopeObj = {};
+			scopeObj[group] = scopes;
+
+			return this.observableToPromise(permissions.push(scopeObj));
+		}
+	}
+
+	// See line 18
+	removeScope(
+		$key: string,
+		type: PermissionsType,
+		scopes: Partial<PermissionsScopes>,
+		group: PermissionsGroup,
+		$uid?: string
+	) {
+		// TODO: figure out how to remove multiple scopes at once
+		if (group === 'user') {
+			// remove scope with $uid handling
 		} else {
 			// add scope normally
 		}
 	}
 
-	removeScope($key: string, scope: Partial<PermissionsScopes>, group: PermissionsGroups, $uid?: string) {
-		if (group === 'user') {
-			// add scope, with $uid handling
-		} else {
-			// add scope normally
-		}
+	private observableToPromise(promise): Observable<any> {
+
+		const subject = new Subject<any>();
+
+		promise
+			.then(res => {
+					subject.next(res);
+					subject.complete();
+				},
+				err => {
+					subject.error(err);
+					subject.complete();
+				});
+
+		return subject.asObservable();
 	}
 
 }
@@ -29,10 +76,10 @@ export class PermissionsService {
 
 export type PermissionsType = 'chat' | 'session' | 'whiteboard';
 
-export type PermissionsGroups = 'anonymous' | 'loggedIn' | 'user';
+export type PermissionsGroup = 'anonymous' | 'loggedIn' | 'user';
 
 export interface Permission {
-	// We can't use [T in keyof PermissionsGroups] here since 'user' is a special case with the extra $uid.
+	// We can't use [T in PermissionsGroup] here since 'user' is a special case with the extra $uid.
 	anonymous: Partial<PermissionsScopes>;
 	loggedIn: Partial<PermissionsScopes>;
 	user: {
