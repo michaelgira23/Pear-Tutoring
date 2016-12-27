79a0dd5ca4fd6bd472b25a724ed3ec122b7d2b9c
Nick Clifford <catcheese12345@gmail.com>
2016-12-27 15:53:37 -0600

    more types, outlined scope handling functions
    the arguments for `addScope` and `removeScope` are arranged as they are since you have to put optional parameters at the end

--- src/app/shared/model/permissions.service.ts
+++ src/app/shared/model/permissions.service.ts
@@ -8,20 +8,35 @@ export class PermissionsService {
 
 	createPermission(type: PermissionsType, permission: Permission) { }
 
-	updatePermission($key: string, permission: Permission) {}
+	addScope($key: string, scope: PermissionsScopes, group: PermissionsGroups, $uid?: string) {
+		if (group === 'user') {
+			// add scope, with $uid handling
+		} else {
+			// add scope normally
+		}
+	}
+
+	removeScope($key: string, scope: PermissionsScopes, group: PermissionsGroups, $uid?: string) {
+		if (group === 'user') {
+			// add scope, with $uid handling
+		} else {
+			// add scope normally
+		}
+	}
 
 }
 
 
 export type PermissionsType = 'chat' | 'session' | 'whiteboard';
 
+export type PermissionsGroups = 'anonymous' | 'loggedIn' | 'user';
+
 export interface Permission {
-	[$key: string]: {
-		anonymous: Partial<PermissionsScopes>;
-		loggedIn: Partial<PermissionsScopes>;
-		user: {
-			[$uid: string]: Partial<PermissionsScopes>;
-		}
+	// We can't use [T in keyof PermissionsGroups] here since 'user' is a special case with the extra $uid.
+	anonymous: Partial<PermissionsScopes>;
+	loggedIn: Partial<PermissionsScopes>;
+	user: {
+		[$uid: string]: Partial<PermissionsScopes>;
 	};
 };
 
