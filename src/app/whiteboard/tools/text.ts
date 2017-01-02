10900a9260212a5ade8ed0e5b09975445f35c763
Michael Gira <contact@michaelgira.me>
2017-01-02 17:32:52 -0600

    Add ability to edit existing text content with text tool

--- src/app/whiteboard/tools/text.ts
+++ src/app/whiteboard/tools/text.ts
@@ -12,6 +12,8 @@ export class Text {
 
 	currentText: any;
 	currentTextFinished: boolean = true;
+	// Whether currentText previously existing on whiteboard
+	editingCurrentText: boolean = false;
 
 	constructor(private whiteboard: WhiteboardComponent) { }
 
@@ -21,19 +23,36 @@ export class Text {
 
 	mousedown(event) {
 		const point = this.whiteboard.cursorPoint(event);
+
 		if (this.currentTextFinished) {
 			this.currentTextFinished = false;
 
-			let paperOptions = styles.deserialize(this.whiteboard.styleOptions);
-			paperOptions.content = 'TEXT';
-			paperOptions.point = point;
+			// Find if text tool hit existing text
+			const hit = paper.project.hitTest(point, {
+				class: paper.PointText,
+				fill: true,
+				stroke: true,
+				bounds: true,
+				tolerance: 5
+			});
 
+			if (!hit) {
+				// We did not hit any text. Create anotha one.
+				let paperOptions = styles.deserialize(this.whiteboard.styleOptions);
+				paperOptions.content = 'TEXT';
+				paperOptions.point = point;
 
-			// Combine paperOptions and fontOptions
-			const fontOptions = font.deserialize(this.whiteboard.fontOptions);
-			paperOptions = Object.assign(paperOptions, fontOptions);
+				// Combine paperOptions and fontOptions
+				const fontOptions = font.deserialize(this.whiteboard.fontOptions);
+				paperOptions = Object.assign(paperOptions, fontOptions);
 
-			this.currentText = new paper.PointText(paperOptions);
+				this.currentText = new paper.PointText(paperOptions);
+				this.editingCurrentText = false;
+			} else {
+				// Select hit text
+				this.currentText = hit.item;
+				this.editingCurrentText = true;
+			}
 		} else if (this.currentText) {
 			this.currentText.point = point;
 		}
@@ -49,25 +68,46 @@ export class Text {
 	mouseup(event) {
 		if (this.currentText && this.whiteboard.allowWrite) {
 			this.currentTextFinished = true;
-			this.currentText.content = prompt('Enter text here:', this.currentText.content);
+
+			if (!this.editingCurrentText) {
+				this.currentText.content = prompt('Enter text here:', this.currentText.content);
+			} else {
+				this.currentText.content = prompt('Edit text here:', this.currentText.content);
+			}
 
 			// Save text
-			const text: WhiteboardTextOptions = {
+			const textOptions: WhiteboardTextOptions = {
 				rotation: this.currentText.rotation,
 				bounds: rectangles.serialize(this.currentText.bounds),
 				content: this.currentText.content,
-				font: this.whiteboard.fontOptions,
-				style: this.whiteboard.styleOptions
+				font: font.serialize(this.currentText),
+				style: styles.serialize(this.currentText)
 			};
-			this.whiteboard.whiteboardService.createText(this.whiteboard.key, text)
-				.subscribe(
-					data => {
-						console.log('successfully added text', data);
-					},
-					err => {
-						console.log('add text error', err);
-					}
-				);
+
+			if (this.editingCurrentText) {
+				// We are editing existing text. Edit old on.
+				const textKey = this.whiteboard.textIdToPushKey(this.currentText.id);
+				this.whiteboard.whiteboardService.editText(this.whiteboard.key, textKey, textOptions)
+					.subscribe(
+						data => {
+							console.log('successfully edited text', data);
+						},
+						err => {
+							console.log('edit text error', err);
+						}
+					);
+			} else {
+				// We are not editing existing text. Create new text.
+				this.whiteboard.whiteboardService.createText(this.whiteboard.key, textOptions)
+					.subscribe(
+						data => {
+							console.log('successfully added text', data);
+						},
+						err => {
+							console.log('add text error', err);
+						}
+					);
+			}
 		}
 	}
 
