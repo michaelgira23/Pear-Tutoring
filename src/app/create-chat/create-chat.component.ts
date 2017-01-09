4f0b619e8c35f322a766bab2768872ebca7b9e41
Nick Clifford <catcheese12345@gmail.com>
2017-01-09 13:25:37 -0600

    add chat creation permissions interface

--- src/app/create-chat/create-chat.component.ts
+++ src/app/create-chat/create-chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { ChatService } from '../shared/model/chat.service';
+import { Permission, PermissionsService } from '../shared/security/permissions.service';
 
 @Component({
 	selector: 'app-create-chat',
@@ -10,7 +11,18 @@ import { ChatService } from '../shared/model/chat.service';
 })
 export class CreateChatComponent implements OnInit {
 
-	constructor(private router: Router, private chatService: ChatService) { }
+	permissions: Permission = {
+		anonymous: {
+			read: true,
+			write: false
+		},
+		loggedIn: {
+			read: true,
+			write: true
+		}
+	};
+
+	constructor(private router: Router, private chatService: ChatService, private permissionsService: PermissionsService) { }
 
 	ngOnInit() {
 	}
@@ -18,9 +30,20 @@ export class CreateChatComponent implements OnInit {
 	create() {
 		this.chatService.createChat().subscribe(
 			data => {
+				let chatKey = data.getKey();
+
 				console.log('New chat room successfully created!');
-				console.log(`New chat room key: ${data.getKey()}`);
-				this.router.navigate(['chat', data.getKey()]);
+				console.log(`New chat room key: ${chatKey}`);
+
+				this.permissionsService.createPermission(chatKey, 'chat', this.permissions).subscribe(
+					data => {
+						console.log(`Created chat permissions: ${data}`);
+						this.router.navigate(['chat', chatKey]);
+					},
+					err => {
+						console.log(`Error creating permissions for new chat room: ${err}`);
+					}
+				);
 			},
 			err => {
 				console.log(`Error creating chat room: ${err}`);
