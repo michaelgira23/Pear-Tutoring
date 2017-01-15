6de94da2fafd5619bc90b048c4a94501fd7ff004
Michael Gira <contact@michaelgira.me>
2017-01-15 00:22:35 -0600

    Prevent landing page from playing animation multiple times simultaneously
    Happened when you visited home page, went to another page, then
    navigated back to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { FirebaseAuthState } from 'angularfire2';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { AuthService } from '../shared/security/auth.service';
 
@@ -9,7 +9,7 @@ import { AuthService } from '../shared/security/auth.service';
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
 	@ViewChild('video')
 	video;
@@ -23,6 +23,8 @@ export class HomeComponent implements OnInit {
 		'pass-your-test',
 		'impact-your-community'
 	];
+	// Whether or not animation timer has already started
+	animationSubscription: any;
 
 	constructor(private authService: AuthService) {
 		this.authService.auth$.subscribe(authInfo => this.authInfo = authInfo);
@@ -37,65 +39,66 @@ export class HomeComponent implements OnInit {
 		}, 150);
 
 		// Loop through SVG text and write
-		this.animateHandwritingSVG(this.animations[0])
+		this.animationSubscription = this.animateHandwritingSVG(this.animations[0])
 			.expand(n => {
 				return this.animateHandwritingSVG(++n % this.animations.length);
 			})
 			.subscribe();
 	}
 
+	ngOnDestroy() {
+		this.animationSubscription.unsubscribe();
+	}
+
 	// Writes and disappears an SVG. Animation time varies depending on how long text is.
-	// Returns an observable that completes when animation finishes. Animation will run independent of subscribing to this observable.
 	animateHandwritingSVG(className: string | number): Observable<any> {
-		// Create subject for emitting observable when finished
-		const subject = new Subject();
+		return Observable.create(observer => {
 
-		// Interval each marking path should be drawn in at (in ms)
-		const interval = 100;
-		// Delay to show SVG after it has been drawn (in ms)
-		const delay = 1000;
+			// Interval each marking path should be drawn in at (in ms)
+			const interval = 100;
+			// Delay to show SVG after it has been drawn (in ms)
+			const delay = 1000;
 
-		// If number, get class name from animations array
-		if (typeof className === 'number') {
-			className = this.animations[className];
-		}
+			// If number, get class name from animations array
+			if (typeof className === 'number') {
+				className = this.animations[className];
+			}
 
-		// Get nth
-		const animationIndex = this.animations.indexOf(className);
+			// Get nth
+			const animationIndex = this.animations.indexOf(className);
 
-		// Get SVG and paths from DOM
-		const svg: SVGElement = <SVGElement>document.getElementsByClassName(className)[0];
-		const paths = svg.getElementsByTagName('path');
+			// Get SVG and paths from DOM
+			const svg: SVGElement = <SVGElement>document.getElementsByClassName(className)[0];
+			const paths = svg.getElementsByTagName('path');
 
-		// Turn of animation for reseting values
-		this.animate = false;
-		this.resetDashArrayOffset(className);
-		// Reset height of animation
-		(<any>svg).style.height = '100%';
+			// Turn of animation for reseting values
+			this.animate = false;
+			this.resetDashArrayOffset(className);
+			// Reset height of animation
+			(<any>svg).style.height = '100%';
 
-		// Iterate through paths and set timeout for drawing path back in
-		for (let i = 0; i < paths.length; i++) {
-			const path = paths[i];
+			// Iterate through paths and set timeout for drawing path back in
+			for (let i = 0; i < paths.length; i++) {
+				const path = paths[i];
+				setTimeout(() => {
+					this.animate = true;
+					path.style.strokeDashoffset = '0';
+				}, interval * i + 5);
+			}
+
+			// Set final interval for shrinking SVG
 			setTimeout(() => {
 				this.animate = true;
-				path.style.strokeDashoffset = '0';
-			}, interval * i + 5);
-		}
+				(<any>svg).style.height = '0';
 
-		// Set final interval for shrinking SVG
-		setTimeout(() => {
-			this.animate = true;
-			(<any>svg).style.height = '0';
+				// Emit observable after height is animated
+				setTimeout(() => {
+					observer.next(animationIndex);
+					observer.complete();
+				}, interval);
 
-			// Emit observable after height is animated
-			setTimeout(() => {
-				subject.next(animationIndex);
-				subject.complete();
-			}, interval);
-
-		}, (interval * paths.length) + delay);
-
-		return subject.asObservable();
+			}, (interval * paths.length) + delay);
+		});
 	}
 
 	resetDashArrayOffset(className: string) {
