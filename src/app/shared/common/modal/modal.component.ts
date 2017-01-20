93fe114881f7b25d0f90cc2257af5d450ff05579
Michael Gira <contact@michaelgira.me>
2017-01-20 11:36:24 -0600

    Properly get modal context even if ngOnInit isn't called yet

--- src/app/shared/common/modal/modal.component.ts
+++ src/app/shared/common/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewChecked, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 declare const dialogPolyfill: any;
 
@@ -7,27 +7,41 @@ declare const dialogPolyfill: any;
 	templateUrl: './modal.component.html',
 	styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements AfterViewChecked {
+export class ModalComponent implements OnInit {
 
 	@ViewChild('modal') modal;
 	modalEl: any;
 
 	// FIXME: this lifecycle hook doesn't fire late enough. it doesn't end up detect the modal element
-	ngAfterViewChecked() {
-		this.modalEl = this.modal.nativeElement;
+	ngOnInit() {
+		// this.modalEl = this.modal.nativeElement;
+		this.initializeModal();
+		console.log('modal element', this.modalEl);
 
 		if (!this.modalEl.showModal) {
 			dialogPolyfill.registerDialog(this.modalEl);
 		}
 	}
 
+	initializeModal() {
+		this.modalEl = this.modal.nativeElement;
+	}
+
 	show() {
-		console.log(this.modalEl);
+		if (!this.modalEl) {
+			this.initializeModal();
+		}
+
+		console.log('show modal', this.modalEl);
 		this.modalEl.showModal();
 	}
 
 	hide() {
-		console.log(this.modalEl);
+		if (!this.modalEl) {
+			this.initializeModal();
+		}
+
+		console.log('hide modal', this.modalEl);
 		this.modalEl.close();
 	}
 
